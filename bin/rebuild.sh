7f09babc0cbb13aa3c52e924ff17efc45f6996dc
mchekavyj <thetangro@gmail.com>
2024-09-09 22:49:35 +0300

    process

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git reset --hard master && git pull
+git reset --hard master && git pull pull master
 composer install --no-dev --no-progress --no-suggest --ignore-platform-reqs
 php bin/console cache:cl
 php bin/console cache:opcache:clear
